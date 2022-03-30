1229e4719933a9f464a36b32d812abcf1848c5cf
Dogifu <maximus2106@mail.ru>
2022-03-30 09:55:20 +0300

    clang-format --style=Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
-    std::string name; // Инициальзируем переменную name типа string
-    std::cout << "Input user_name"; 
-    std::cin >> name; // получаем имя пользователя
-    std::cout << "Hello world from " << name << std::endl; // Выводим преветствие
-    return 0;
+  std::string name; // Инициальзируем переменную name типа string
+  std::cout << "Input user_name";
+  std::cin >> name; // получаем имя пользователя
+  std::cout << "Hello world from " << name << std::endl; // Выводим преветствие
+  return 0;
 }
